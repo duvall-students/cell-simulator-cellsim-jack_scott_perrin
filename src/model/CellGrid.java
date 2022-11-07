444ec3926197f4ac17c43a8afc36a7ecf594425f
scottsumrall <ssumrall@elon.edu>
2022-11-07 13:56:42 -0500

    Added some notes for cellgrid class planning

--- src/model/CellGrid.java
+++ src/model/CellGrid.java
@@ -1,5 +1,10 @@
 package model;
 
-public class CellGrid {
-
+public class CellGrid 
+{
+	/*
+	 * create 2d array based on size for cell storage
+	 * initialize cellgrid with random cells and empty edge cells
+	 * function to return neighbors
+	*/
 }
