07e2eb47b675740325356bf2b24dc2adba813bf0
snsum <snsum@10.112.221.191>
2022-11-07 13:32:12 -0500

    Controller base class

---
+++ src/controller/AutomataController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class AutomataController 
+{
+	
+	//get the stage from the view and return it
+	
+	//update method, get all cells from cellGrid and call update on them
+}
