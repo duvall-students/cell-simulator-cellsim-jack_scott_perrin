a99a55926cb347efaf3137b474752c78674f3f8f
scottsumrall <ssumrall@elon.edu>
2022-11-12 17:35:30 -0500

    Refactored main moving application start point to view

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,24 +2,12 @@ package main;
 import controller.AutomataController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import view.AutomataView;
 
-public class Main extends Application
+public class Main
 {
-
-	@Override
-	public void start(Stage primaryStage)
-	{
-			
-
-			AutomataController rpsCellController = new AutomataController();
-
-			//primaryStage = rpsCellController.getStage();
-
-			//primaryStage.show();
-	}
-	
 	public static void main(String args[]){ 	
-		  launch(args);
+		  AutomataView.main(args);
 	}
 }
 
