426dc232419aa6b2afb5a1aca9a6af0b514a9d38
scottsumrall <ssumrall@elon.edu>
2022-11-12 17:29:08 -0500

    Made class concrete, added starter methods

---
+++ src/model/AutomataCell.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.awt.Point;
+
+import javafx.scene.paint.Color;
+
+public class AutomataCell 
+{
+	public AutomataCell()
+	{
+		
+	}
+	
+	public Color getColor()
+	{
+		return null;
+	}
+	
+	public Point getCoordinates()
+	{
+		return null;
+	}
+}
