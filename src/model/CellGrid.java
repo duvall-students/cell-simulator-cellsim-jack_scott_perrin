aabd232a86f08553cb0e7ad18b6d0f0d052f90d1
scottsumrall <ssumrall@elon.edu>
2022-11-12 17:34:36 -0500

    Added skeleton methods that will be called from controller

--- src/model/CellGrid.java
+++ src/model/CellGrid.java
@@ -1,10 +1,40 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import controller.RPSHandler;
+import javafx.scene.control.Cell;
+
 public class CellGrid 
 {
-	/*
-	 * create 2d array based on size for cell storage
-	 * initialize cellgrid with random cells and empty edge cells
-	 * function to return neighbors
-	*/
+	private static final int THRESHOLD = 3;
+
+	public CellGrid(RPSHandler handler) 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public void generateGrid(int width, int height) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Collection<AutomataCell> getChangedCells() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateBoard() 
+	{
+		//update board using handler
+		/*
+		 * for each cell in the grid
+		 * that cell = handler.compareCellToNeighbors(cell, neighborCells[])
+		 * the handler applies rules and returns the state of the given cell
+		 */
+		
+	}
 }
