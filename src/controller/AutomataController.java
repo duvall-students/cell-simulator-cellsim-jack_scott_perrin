445955581f8f4fd4c62452cb5934c3a5fcf6ddd1
scottsumrall <ssumrall@elon.edu>
2022-11-12 17:30:01 -0500

    Took initialization out of constructor, added input handlers for view

--- src/controller/AutomataController.java
+++ src/controller/AutomataController.java
@@ -1,65 +1,104 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Cell;
 import javafx.util.Duration;
-import model.CellGrid;
+import model.*;
 import view.AutomataView;
 
 public class AutomataController 
 {
 	private final int MILLISECOND_DELAY = 15;
+	public IntegerProperty gridWidth = new SimpleIntegerProperty();
+	public IntegerProperty gridHeight = new SimpleIntegerProperty();
+	private boolean paused = true;
 	
 	CellGrid cellGrid;
-	private int gridWidth;
-	private int gridHeight;
+	AutomataView automataView;
+	boolean automataInitialized = false;
 
 	public AutomataController(AutomataView automataView)
 	{
+		this.automataView = automataView;
+	}
+	
+	/**
+	 * Generate dependencies for automata controller functionality
+	 */
+	public void initialize()
+	{		
+		//create handler that will determine rules for the automata
 		RPSHandler handler = new RPSHandler();
 		cellGrid = new CellGrid(handler);
+	
+		automataView.generateInitialView();
 		
-		//generate button 
-		cellView.generateInitialView();
-		
-		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> mazeController.step(MILLISECOND_DELAY));
+		//initialize the automata step method to update
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(MILLISECOND_DELAY));
 		Timeline animation = new Timeline();
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.getKeyFrames().add(frame);
-		animation.play();
+		animation.play();	
 	}
 	
-	private void step()
+	private void step(double timeElapsed)
 	{
+		if(!paused)
+		{
+			doOneStep(timeElapsed);
+		}
+	}
+	
+	private void colorCells(Collection<AutomataCell> changedCells)
+	{
+
+		for(AutomataCell changedCell : changedCells)
+		{
+			automataView.colorCell(changedCell.getColor(), changedCell.getCoordinates());
+		}
+	}
+	
+	/**
+	 * Create new automata grid/view
+	 */
+	public void generateNewAutomata()
+	{
+		int width = gridWidth.getValue();
+		int height = gridWidth.getValue();
 		
+		if(width > 0 && height > 0)
+		{
+			automataView.generateGridView(width, height);
+			cellGrid.generateGrid(width, height);
+		}
+	}
+	
+	/**
+	 * perform a single iteration of the automata
+	 * @param timeElapsed
+	 */
+	public void doOneStep(double timeElapsed)
+	{
 		cellGrid.updateBoard();
 		
-		ArrayList<Cell> changedCells = cellGrid.getChangedCells();
-		
-		colorCells(changedCells);
-		
-		
+		colorCells(cellGrid.getChangedCells());
 	}
-	
 
-	private void colorCells(changedCells)
+
+	/**
+	 * handler for pause/play button
+	 */
+	public void handlePause()
 	{
-		for(Cell changedCell : changedCells)
-		{
-			AutomataView.colorCell(changedCell.getColor, changedCell.getCoordinate)
-		}
+		paused = !paused;
+		automataView.updatePauseButton(paused);
 	}
 	
-	private void generateCellGrid()
-	{
-		if(gridWidth > 0 && gridHeight > 0)
-		{
-			automataView.initializeView(gridWidth, gridHeight);
-			cellGrid.initializeGrid(gridWidth, gridHeight);
-		}
-		
-	}
+	
 }
